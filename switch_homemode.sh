84798f02d5a9c52ef980280a70b6e15be293bb11
Gioxx <dev@gfsolone.com>
2018-12-24 12:33:21 +0100

    Correzioni
    Mancava il blocco dei file di stato e il lancio dello script PHP (non ho
    ben chiaro dove lo avessi perso ma tant'è).

--- switch_homemode.sh
+++ switch_homemode.sh
@@ -13,6 +13,11 @@ CHAT_ID="-111111111";
 MSG_SS_ACTIVE="Synology Surveillance Activated. I'm recording.";
 MSG_SS_INACTIVE="Synology Surveillance Deactivated. I'm not recording anymore.";
 
+######## State file configuration (don't touch anything if not necessary)
+STATEFILE='/var/services/homes/api_user/synohomemode.state';
+RETRYFILE='/var/services/homes/api_user/synohomemode.retry';
+CHECKFRITZ='/usr/local/bin/php70 /var/services/homes/api_user/fritz_activemac.php';
+
 ######### Internal variables ############
 MACS=$@;
 ID="$RANDOM";
@@ -21,12 +26,12 @@ COOKIESFILE="$0-cookies-$ID";
 ###### Functions
 function _switchHomemode ()
 {
-wget -q --keep-session-cookies --save-cookies $COOKIESFILE -O- "http://${SYNO_URL}//webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=${SYNO_SS_USER}&passwd=${SYNO_SS_PASS}&session=SurveillanceStation";
-wget -q --load-cookies $COOKIESFILE -O- "http://${SYNO_URL}//webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&on=${1}";
-wget -q --load-cookies $COOKIESFILE -O- "http://${SYNO_URL}/webapi/auth.cgi?api=SYNO.API.Auth&method=Logout&version=1";
-rm $COOKIESFILE;
-rm $STATEFILE;
-echo $1 > $STATEFILE;
+	wget -q --keep-session-cookies --save-cookies $COOKIESFILE -O- "http://${SYNO_URL}//webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=${SYNO_SS_USER}&passwd=${SYNO_SS_PASS}&session=SurveillanceStation";
+	wget -q --load-cookies $COOKIESFILE -O- "http://${SYNO_URL}//webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&on=${1}";
+	wget -q --load-cookies $COOKIESFILE -O- "http://${SYNO_URL}/webapi/auth.cgi?api=SYNO.API.Auth&method=Logout&version=1";
+	rm $COOKIESFILE;
+	rm $STATEFILE;
+	echo $1 > $STATEFILE;
 }
 
 if [ $# -eq 0 ]; then
@@ -38,9 +43,21 @@ if [ -f $STATEFILE ]; then
 	result=$($CHECKFRITZ $FRITZ_URL $MACS);
 	if [ $result -eq 1 ] && grep -q false $STATEFILE; then
 		_switchHomemode "true";
+		if [ $TELEGRAM -eq 1 ]; then
+			echo ""; echo ""; echo "Syno SS Status: INACTIVE | Send updated status to Telegram ...";
+			TGRAM_COMMAND="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${MSG_SS_INACTIVE}";
+			curl -L "$TGRAM_COMMAND";
+			echo ""; echo ""; 
+		fi
 	elif [ -f $RETRYFILE ]; then
 		if [ $result -eq 0 ] && grep -q true $STATEFILE; then
 			_switchHomemode "false";
+			if [ $TELEGRAM -eq 1 ]; then
+				echo ""; echo ""; echo "Syno SS Status: ACTIVE | Send updated status to Telegram ...";
+				TGRAM_COMMAND="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${MSG_SS_ACTIVE}";
+				curl -L "$TGRAM_COMMAND";
+				echo ""; echo ""; 
+			fi
 			rm $RETRYFILE;
 		else
 			rm $RETRYFILE;
@@ -52,3 +69,4 @@ else
 	echo false > $STATEFILE;
 fi
 exit 0;
+
